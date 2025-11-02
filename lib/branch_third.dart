9b45e01ad24b28f676ce40464a6f3ff5686a2f2c
Tapan <tapanmehta708@gmail.com>
2025-11-02 15:18:11 +0530

    Make an third screen

---
+++ lib/branch_third.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BranchThird extends StatefulWidget {
+  const BranchThird({super.key});
+
+  @override
+  State<BranchThird> createState() => _BranchThirdState();
+}
+
+class _BranchThirdState extends State<BranchThird> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
