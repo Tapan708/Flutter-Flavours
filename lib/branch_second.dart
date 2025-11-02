f6a1e291b9d904a06bb9e5c001bc195ecb473420
Tapan <tapanmehta708@gmail.com>
2025-11-02 15:01:23 +0530

    Make an Second branch just screen

---
+++ lib/branch_second.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BranchSecond extends StatefulWidget {
+  const BranchSecond({super.key});
+
+  @override
+  State<BranchSecond> createState() => _BranchSecondState();
+}
+
+class _BranchSecondState extends State<BranchSecond> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
